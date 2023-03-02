4dd9159d26cfbb997e2919e820ff68f54c7fc665
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2023-03-02 09:29:53 -0300

    Add files via upload

---
+++ MeuPrimeiroProjeto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.meuprimeiroprojeto;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class MeuPrimeiroProjeto {
+
+    public static void main(String[] args) {
+        if (10 > 20){
+        System.out.println("Hello World!");
+        }
+        else {
+            System.out.println("Olá Unipar");
+        }
+    }
+}
