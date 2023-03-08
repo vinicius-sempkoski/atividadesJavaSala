fac6df7fe1a2c548946ebb07008c48e8ae5febb1
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2023-03-08 20:09:42 -0300

    Atividade1POOSala
    Criar uma classe distinguindo 5 atributos de um animal, e depois fazer uma classe para cadastrar 5 animais.

---
+++ Cachorro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividadessala;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class Cachorro {
+    
+        public String raca;
+        public String cor;
+        public String porte;
+        public int idade;
+        public int peso;
+        
+}
