de5f5b277a652a01b65e8774b76c85b9cf5fc4d1
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2023-03-08 21:12:52 -0300

    Add files via upload

--- Cachorro.java
+++ Cachorro.java
@@ -15,5 +15,18 @@ public class Cachorro {
         public String porte;
         public int idade;
         public int peso;
+        public int tamanho;
+        
+        public void latir(){
+            if(tamanho > 60){
+                System.out.println("Wooof, Wooof!");
+            }
+            else if (tamanho > 14){
+                System.out.println("Ruff, Ruff!");
+            }
+            else{
+                System.out.println("Yipi, Yipi!");
+            }
+        }
         
 }
--- testeCachorro.java
+++ testeCachorro.java
@@ -15,6 +15,8 @@ public class testeCachorro {
         cachorroFilho.peso = 30;
         cachorroFilho.cor = "Capa Preta";
         cachorroFilho.porte = "Grande";
+        cachorroFilho.tamanho = 70;
+        cachorroFilho.latir();
 
         Cachorro cachorroFilho2 = new Cachorro();
 
@@ -23,6 +25,8 @@ public class testeCachorro {
         cachorroFilho2.peso = 5;
         cachorroFilho2.cor = "Preto";
         cachorroFilho2.porte = "Pequeno";
+        cachorroFilho2.tamanho = 5;
+        cachorroFilho2.latir();
 
         Cachorro cachorroFilho3 = new Cachorro();
 
@@ -31,6 +35,8 @@ public class testeCachorro {
         cachorroFilho3.peso = 15;
         cachorroFilho3.cor = "Preto e branco";
         cachorroFilho3.porte = "Grande";
+        cachorroFilho3.tamanho = 62;
+        cachorroFilho3.latir();
 
         Cachorro cachorroFilho4 = new Cachorro();
 
@@ -39,6 +45,8 @@ public class testeCachorro {
         cachorroFilho4.peso = 10;
         cachorroFilho4.cor = "Preto";
         cachorroFilho4.porte = "Medio";
+        cachorroFilho4.tamanho = 16;
+        cachorroFilho4.latir();
 
         Cachorro cachorroFilho5 = new Cachorro();
 
@@ -47,6 +55,8 @@ public class testeCachorro {
         cachorroFilho5.peso = 4;
         cachorroFilho5.cor = "Marrom";
         cachorroFilho5.porte = "Pequeno";
+        cachorroFilho5.tamanho = 7;
+        cachorroFilho5.latir();
     }
     
 }
